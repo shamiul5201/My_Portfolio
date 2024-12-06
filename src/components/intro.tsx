99b096d30f2256737f3149563b39ea64ff2fc148
MD. SHAMIUL ISLAM <68953368+shamiul5201@users.noreply.github.com>
2024-12-06 17:44:18 +0000

    update intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -11,7 +11,6 @@ import { useSectionInView } from '@/lib/hooks';
 import { useActiveSectionContext } from '@/src/context/active-section-context';
 import myDP from "@/public/my-dp.png";
 
-
 export default function Intro() {
     const { ref } = useSectionInView('Home', 0.5);
     const {
@@ -52,44 +51,40 @@ export default function Intro() {
                 animate={{ opacity: 1, y: 0 }}
                 whileHover={{ scale: 1.05 }}
                 transition={{ duration: 0.3 }}
-                >
-                <span className="font-bold">Hi, I’m Shamiul Islam</span>—a passionate Computer Vision and AI Engineer turning 
+            >
+                <span className="font-bold">Hi, I&apos;m Shamiul Islam</span>—a passionate Computer Vision and AI Engineer turning 
                 complex challenges into cutting-edge solutions. I transform innovation into impact, building intelligent 
-                systems that shape the future. <span className="font-medium">Let's create something extraordinary.</span>
+                systems that shape the future. <span className="font-medium">Let&apos;s create something extraordinary.</span>
             </motion.h1>
 
-
-
-
-
-<motion.div className='flex flex-col sm:flex-row items-center justify-center gap-4 px-6 text-lg font-medium'
-    initial={{ opacity: 0, y: 100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ delay: 0.1 }}
->
-    <Link href="#contact" className='group bg-gray-900 text-white px-8 py-3 flex items-center gap-3 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition mb-3 sm:mb-0'
-        onClick={() => {
-            setActiveSection("Contact");
-            setTimeOfLastClick(Date.now());
-        }}
-    >
-        Contact me here
-        <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
-    </Link>
-    <a className="group bg-white px-8 py-3 flex items-center gap-3 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-gray-900 dark:bg-white/10 dark:hover:bg-white/15 mb-3 sm:mb-0" href='/SHAMIUL_ISLAM.pdf' download>
-        Download Resume <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
-    </a>
-    <div className='flex flex-row items-center justify-center gap-6 sm:gap-3'>
-        <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-gray-900 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/15 dark:hover:text-white/70'
-            href='https://www.linkedin.com/in/sham-islam' target='_blank'>
-            <BsLinkedin />
-        </a>
-        <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-gray-900 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/15 dark:hover:text-white/70' href='https://github.com/shamiul5201' target='_blank'>
-            <FaGithubSquare />
-        </a>
-    </div>
-</motion.div>
+            <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-4 px-6 text-lg font-medium'
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.1 }}
+            >
+                <Link href="#contact" className='group bg-gray-900 text-white px-8 py-3 flex items-center gap-3 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition mb-3 sm:mb-0'
+                    onClick={() => {
+                        setActiveSection("Contact");
+                        setTimeOfLastClick(Date.now());
+                    }}
+                >
+                    Contact me here
+                    <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+                </Link>
+                <a className="group bg-white px-8 py-3 flex items-center gap-3 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-gray-900 dark:bg-white/10 dark:hover:bg-white/15 mb-3 sm:mb-0" href='/SHAMIUL_ISLAM.pdf' download>
+                    Download Resume <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+                </a>
+                <div className='flex flex-row items-center justify-center gap-6 sm:gap-3'>
+                    <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-gray-900 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/15 dark:hover:text-white/70'
+                        href='https://www.linkedin.com/in/sham-islam' target='_blank'>
+                        <BsLinkedin />
+                    </a>
+                    <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-gray-900 dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/15 dark:hover:text-white/70' href='https://github.com/shamiul5201' target='_blank'>
+                        <FaGithubSquare />
+                    </a>
+                </div>
+            </motion.div>
 
         </section>
-    )
+    );
 }
